8cff6207b65dd6573d84dca814cd3109af73196f
Robin Tapiru <robintapiru0894@gmail.com>
2024-10-12 02:04:44 +0400

    Search component (controlled input)

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
   return (
-    <form action="#" className="search">
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+      action="#"
+      className="search"
+    >
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(event) => {
+          setSearchText(event.target.value);
+          console.log(searchText);
+        }}
         spellCheck="false"
         type="text"
         required
